81a1885c02744426d9fa29a076f2596db4aee56a
Cedric <crubbertire@rogers.com>
2025-11-20 17:39:19 -0500

    Set up inventory system

---
+++ game.py
@@ -0,0 +1,12 @@
+# Landscaper
+
+## You are starting a landscaping business, but all you have are your teeth.
+## Using just your teeth, you can spend the day cutting lawns and make $1. You can do this as much as you want.
+## As you cut grass, and accumulate money... You may chose to purchase new lawncutting tools! These tools will increase your income per lawn!
+
+# I'll be using tupples to store values for the tools available and tool price as those will remain fixed values
+toolsAvailable = ("teeth", "rusty scissors", "push lawnmower", "battery-powered lawnmower", "students")
+toolPrice = (0, 25, 50, 250, 500)
+# Tools owned, however, will be set as a list since there can be many of each kind owned.
+toolsOwned = [1, 0, 0, 0, 0]
+
